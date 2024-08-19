89319691a1a0100d3e88b9983ce3fa245a5e5691
PraveenN1 <praveenpu46@gmail.com>
2024-08-19 19:33:35 +0530

    added components

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-around items-center  bg-white border-2 p-2">
+    <nav className="flex mx-36 my-8 items-center justify-center border-2 p-2 backdrop-blur-md space-x-40 bg-slate-200 rounded-full absolute w-[80%] ">
       <Link to="/">
         {/* <img
           src={''}
           alt="Logo"
           className="w-12 h-12 mx-5"
         /> */}
-        <p className=' '>PRIME FIT</p>
+        <p className='font-bold px-5 py-1 border-2 absolute left-2 top-1 border-red-600 rounded-full'>PRIME FIT</p>
       </Link>
-      <div className="flex gap-10 text-lg">
+      <div className="flex gap-10 text-lg font-semibold">
         <Link
           to="/"
-          className="text-brown-900  border-red-600 hover:text-red-600"
+          className="border-red-600 hover:text-red-600"
         >
           Home
         </Link>
